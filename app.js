60f2f460422b8acee13f51c6275c3a9cdb463720
YD123456789 <a8111202003@gmail.com>
2023-12-11 17:13:10 +0800

    modify file structure

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs'}))
 app.set('view engine', 'hbs')
-
+// 可解析POST表單內容 req.body
+app.use(express.urlencoded({ extended: true }))
+// 增加可覆蓋的PUT DELETE方法
+app.use(methodOverride('_method'))
 app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => {
@@ -13,3 +17,4 @@ app.listen(port, () => {
 })
 
 //google map功能>登入>(金流)
+// Maps JavaScript API  取得目前位置與地圖 
