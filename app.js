ff252180b0032f7babc39899d04da9a438eb4d10
YD123456789 <a8111202003@gmail.com>
2024-01-10 14:38:19 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const port = process.env.PORT || 3000
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs'}))
 app.set('view engine', 'hbs')
--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,18 +1,9 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const User = require('../user')
+ 
+const db = require('../../config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', async () => {
-  console.log('mongodb connected!')
   for (let i = 1; i < 3; i++){
     User.create({
       email: `user${i}@example.com`,
